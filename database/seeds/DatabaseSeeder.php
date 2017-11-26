67e634661d26df56572c4449bea81af6f8248873
Karl T. Macadangdang <karljarren0308@gmail.com>
2017-11-26 18:23:11 +0800

    Added ClientSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(EmployeeSeeder::class);
+        $this->call(ClientSeeder::class);
         $this->call(ProductSeeder::class);
     }
 }
