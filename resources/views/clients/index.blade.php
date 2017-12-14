851a39e4b2a76142b9004788894d1fbb8dd956bc
Karl T. Macadangdang <karljarren0308@gmail.com>
2017-12-14 17:31:39 +0800

    Removed alien texts

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -6,9 +6,7 @@
     </div>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-sm-8">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam ad perferendis, esse quod corporis non est. Debitis, vitae provident ea, rerum tenetur, eveniet esse est qui ducimus voluptatem, sunt odit!</p>
-            </div>
+            <div class="col-sm-8"></div>
             <div class="col-sm-4">
                 <div class="alert alert-success">
                     <h4 data-load="date" class="no-margin"></h4>
