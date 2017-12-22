4a986036c47b8d023bc1a7c9256939c7c8c3320d
Karl T. Macadangdang <karljarren0308@gmail.com>
2017-12-23 03:10:33 +0800

    Changed

--- resources/views/home/test.blade.php
+++ resources/views/home/test.blade.php
@@ -1,32 +1,27 @@
-@extends('layouts.employees')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('company.name') }}</title>
+</head>
+<body>
+    <?php
+        use App\Http\Controllers\PhpSerial;
 
-@section('content')
-    <div class="page-header">
-        <h1 class="no-margin">Test Page</h1>
-    </div>
-    <div class="container-fluid">
-        <?php
-            /*use lepiaf\SerialPort\SerialPort;
-            use lepiaf\SerialPort\Parser\SeparatorParser;
-            use lepiaf\SerialPort\Configure\TTYConfigure;
+        $serial = new PhpSerial();
+        $serial->_os = strtolower(env('PC_OS'));
 
-            $configure = new TTYConfigure();
+        $serial->deviceSet('COM5');
+        $serial->confBaudRate(9600);
+        $serial->deviceOpen('w+');
 
-            $configure->removeOption('9600');
-            $configure->setOption(env('COM_BAUD_RATE'));
+        stream_set_timeout($serial->_dHandle, 10);
 
-            $serialPort = new SerialPort(new SeparatorParser(), $configure);
+        $serial->sendMessage('AT', 1);
+        echo $serial->readPort();
 
-            $serialPort->open(env('COM_PORT'));
-
-            while($data = $serialPort->read()) {
-                if($data === 'OK') {
-                    $serialPort->write('AT+CMGF=1\r');
-                    $serialPort->write('AT+CMGS="' . $phoneNumber . '"\r' . $message . chr(26) . '\r');
-                    $serialPort->close();
-                }
-            }*/
-        ?>
-        <div class="alert alert-info">Check your phone for SMS.</div>
-    </div>
-@endsection
+        $serial->deviceClose();
+    ?>
+</body>
+</html>
