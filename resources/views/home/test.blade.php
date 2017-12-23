120d915786ef24e42e4baa22d0ef444608afb661
Karl T. Macadangdang <karljarren0308@gmail.com>
2017-12-24 06:44:26 +0800

    Experimenting with AT Commands using PHP

--- resources/views/home/test.blade.php
+++ resources/views/home/test.blade.php
@@ -1,41 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('company.name') }}</title>
-</head>
-<body>
-    <div>
-        <?php
-            use App\Http\Controllers\PhpSerial;
+<?php
+    use App\Http\Controllers\PhpSerial;
 
-            $data = '';
+    function isOk($formula) {
+        return ($formula ? 'OK' : 'ERROR');
+    }
 
-            $serial = new PhpSerial();
-            $serial->_os = strtolower(env('PC_OS'));
+    $data = '';
+    $comPort = 'COM6';
+    $baud = env('COM_BAUD_RATE', 9600);
 
-            $serial->deviceSet(env('COM_PORT'));
-            $serial->confBaudRate(env('COM_BAUD_RATE'));
-            $serial->confParity('none');
-            $serial->confCharacterLength(8);
-            $serial->confStopBits(1);
-            $serial->confFlowControl('none');
-            $serial->deviceOpen('w+');
+    $serial = new PhpSerial;
+    $serial->_os = strtolower(env('PC_OS'));
 
-            $serial->sendMessage("AT+CMGF=1" . chr(13), 2);
-            $data .= $serial->readPort();
-            $serial->sendMessage("AT+CMGS=\"" . $phoneNumber . "\"" . chr(13), 2);
-            $data .= $serial->readPort();
-            $serial->sendMessage("This is a sample message. Sent by " . config('company.name') . ".", 2);
-            $data .= $serial->readPort();
-            $serial->sendMessage(chr(26));
-            $data .= $serial->readPort();
+    $serial->deviceSet($comPort);
+    // $serial->confBaudRate(115200);
+    // $serial->confCharacterLength(8);
+    // $serial->confStopBits(1);
+    // $serial->confParity('none');
+    // $serial->confFlowControl('none');
+    $serial->_exec('mode ' . $comPort . ' BAUD=' . $baud . ' PARITY=n DATA=8 STOP=1 xon=off octs=off rts=off dtr=on');
+    $isOpen = $serial->deviceOpen();
 
-            $serial->deviceClose();
+    if($isOpen) {
+        // $serial->sendMessage("AT+CMGF=1" . chr(13), 2);
+        // $data .= $serial->readPort();
+        // $serial->sendMessage("AT+CMGS=\"" . $phoneNumber . "\"" . chr(13), 2);
+        // $data .= $serial->readPort();
+        // $serial->sendMessage("This is a sample message. Sent by " . config('company.name') . ".", 2);
+        // $data .= $serial->readPort();
+        // $serial->sendMessage(chr(26));
+        // $data .= $serial->readPort();
 
-            var_dump($data);
-        ?>
-    </div>
-</body>
-</html>
+        $serial->sendMessage("AT+CMGF=1\r\n", 3);
+        $serial->sendMessage("AT+CMGS=\"" . $phoneNumber . "\"\r\n", 3);
+        $serial->sendMessage("Bal\r\n", 3);
+        $serial->sendMessage(chr(26) . "\r\n", 3);
+
+        sleep(6);
+        var_dump($serial->readPort());
+
+        $serial->deviceClose();
+    } else {
+        echo 'Unable to open device.';
+    }
+?>
