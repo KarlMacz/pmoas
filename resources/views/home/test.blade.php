cd9f28f2bd5bbe54be39833975d3d06f29dc4881
Karl T. Macadangdang <karljarren0308@gmail.com>
2018-01-19 19:06:21 +0800

    New Update(s)

--- resources/views/home/test.blade.php
+++ resources/views/home/test.blade.php
@@ -1,38 +1,3 @@
 <?php
-    use App\Http\Controllers\PhpSerial;
-
-    function isOk($formula) {
-        return ($formula ? 'OK' : 'ERROR');
-    }
-
-    $serial = new PhpSerial();
-    $serial->_os = 'windows';
-
-    $serial->deviceSet('COM4');
-    $serial->confBaudRate(9600);
-    $serial->deviceOpen('w+');
-
-    stream_set_timeout($serial->_dHandle, 10);
-
-    $serial->sendMessage("AT+CREG=1\n\r");
-
-    var_dump($serial->readPort());
-    echo '<br>';
-
-    $serial->sendMessage("AT+CMGF=1\n\r", 2);
-
-    var_dump($serial->readPort());
-    echo '<br>';
-
-    $serial->sendMessage("AT+CMGS=\"09068563348\"\n\r", 2);
-    
-    var_dump($serial->readPort());
-    echo '<br>';
-
-    $serial->sendMessage("Test Message. " . date('Y-m-d H:i:s') . chr(26), 2);
-    
-    var_dump($serial->readPort());
-    echo '<br>';
-
-    $serial->deviceClose();
+    // Scrap hyperthese branch. PhpSerial not working.
 ?>
