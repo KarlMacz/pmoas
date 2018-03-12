ba62389ea0a801e6ec823c6001711f2576542a47
Karl T. Macadangdang <karljarren0308@gmail.com>
2018-03-12 13:13:08 +0800

    Added seed for suppliers table

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 
 use App\Products;
 use App\Stocks;
+use App\Suppliers;
 
 class ProductSeeder extends Seeder
 {
@@ -14,7 +15,11 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        $productId = Products::create([
+        $supplier = Suppliers::create([
+            'name' => 'Unilever'
+        ]);
+
+        $product = Products::create([
             'name' => 'Pond\'s Men Energy Charge Face Wash',
             'description' => 'Get set to give your skin a charge of energy! Formulated with potent ingredients such as coffee bean extracts & cooling menthol, Pond\'s Men Energy Charge face wash energizes tired, haggard skin to give your face a healthy and active glow.',
             'price_per_piece' => 150.00,
@@ -24,13 +29,14 @@ class ProductSeeder extends Seeder
         ]);
 
         Stocks::create([
-            'product_id' => $productId->id,
+            'product_id' => $product->id,
+            'supplier_id' => $supplier->id,
             'quantity' => 100,
             'total_amount' => 12500.00,
             'expiration_date' => '2018-12-30'
         ]);
 
-        $productId = Products::create([
+        $product = Products::create([
             'name' => 'Pond\'s Men White Boost Spot Clearing Scrub',
             'description' => 'Give your skin an instant whitening boost! Pond\'s Men White Boost Spot Clearing Scrub combines the goodness of White Ginseng Extract & Deep Scrub beads that rejuvenate and refresh the skin making it look bright & clear instantly.',
             'price_per_piece' => 150.00,
@@ -40,7 +46,8 @@ class ProductSeeder extends Seeder
         ]);
 
         Stocks::create([
-            'product_id' => $productId->id,
+            'product_id' => $product->id,
+            'supplier_id' => $supplier->id,
             'quantity' => 100,
             'total_amount' => 12500.00,
             'expiration_date' => '2018-12-30'
