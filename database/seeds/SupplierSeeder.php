e47ed44faa0bb50debff1dff018ae86d0ca7858b
Karl T. Macadangdang <karljarren0308@gmail.com>
2018-03-12 13:14:30 +0800

    Seeder for suppliers table

---
+++ database/seeds/SupplierSeeder.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Suppliers;
+
+class SupplierSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /*Suppliers::create([
+            'name' => 'Kyowa Hakko Bio Co. Ltd.'
+        ]);
+
+        Suppliers::create([
+            'name' => 'Mitsuya Boeki'
+        ]);
+
+        Suppliers::create([
+            'name' => 'Daiichi Fine Chemicals'
+        ]);
+
+        Suppliers::create([
+            'name' => 'Aroma NZ Ltd.'
+        ]);
+
+        Suppliers::create([
+            'name' => 'TSI Health Sciences'
+        ]);
+
+        Suppliers::create([
+            'name' => 'Nippi Inc.'
+        ]);
+
+        Suppliers::create([
+            'name' => 'Primex EHF'
+        ]);
+
+        Suppliers::create([
+            'name' => 'Hamari Fine Chemicals'
+        ]);
+
+        Suppliers::create([
+            'name' => 'Exotic Natural'
+        ]);
+
+        Suppliers::create([
+            'name' => 'Amax NutraSource Inc.'
+        ]);
+
+        Suppliers::create([
+            'name' => 'Advanced Protein Systems'
+        ]);
+
+        Suppliers::create([
+            'name' => 'Huachengbio'
+        ]);
+
+        Suppliers::create([
+            'name' => 'Nippon Soda Co., Ltd.'
+        ]);
+
+        Suppliers::create([
+            'name' => 'NP Nutra'
+        ]);*/
+    }
+}
