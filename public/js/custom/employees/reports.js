80a0281fa7db5f0836a58823096762802d6350e9
Karl T. Macadangdang <karljarren0308@gmail.com>
2018-03-12 13:51:01 +0800

    Added button onclick event for generating reports

--- public/js/custom/employees/reports.js
+++ public/js/custom/employees/reports.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+    $('.generate-report-button').click(function() {
+        delayOpenModal('loading-modal', 'static');
+
+        ajaxRequest('/reports/generate/' + $(this).data('type'), 'GET', {}, function(response) {
+            closeModal('loading-modal');
+
+            location.reload();
+        });
+    });
+
     $('.view-report-button').click(function() {
         setModalContent('status-modal', 'View Report', '<iframe src="' + $('meta[name="main-route"]').attr('content') + '/reports/view/' + $(this).data('type') + '/' + $(this).data('id') + '" frameborder="0" style="height: 400px; width: 100%;"></iframe>');
         delayOpenModal('status-modal');
