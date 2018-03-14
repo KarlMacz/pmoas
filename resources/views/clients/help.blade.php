acf8968a47ae73c1dcfb545858aa9b6dcaf2a7fd
Karl T. Macadangdang <karljarren0308@gmail.com>
2018-03-14 12:09:15 +0800

    Updated content

--- resources/views/clients/help.blade.php
+++ resources/views/clients/help.blade.php
@@ -7,18 +7,16 @@
     </div>
     <div class="container-spacious">
         <div class="list-group">
-            <div class="list-group-item">
-                <h4>Q. How will I know that you have received my order?</h4>
-                <p>A. After you complete the checkout process, a receipt will appear on your screen detailing your final order including shipping charges, your billing address, and the items that are being sent to your shipping address. Please keep this receipt for your reference. And a You will also receive confirmation via SMS that we have received your order.</p>
-            </div>
-            <div class="list-group-item">
-                <h4>Q. What are your payment options?</h4>
-                <p>A. We accept Paypal and Cash On Delivery (COD) as well as checks.</p>
-            </div>
-            <div class="list-group-item">
-                <h4>Q. How do I track my orders?</h4>
-                <p>A. You will receive an SMS notification, together with a tracking number, notifying you that orders have been shipped. If you have further queries, please contact us at http://www.eingredientsspecialist.com/#contact-us-section</p>
-            </div>
+            @if($helps->count() > 0)
+                @foreach($helps as $help)
+                    <div class="list-group-item">
+                        <h4>{{ $help->question }}</h4>
+                        <p>{{ $help->answer }}</p>
+                    </div>
+                @endforeach
+            @else
+                <div class="list-group-item text-center">No results found.</div>
+            @endif
         </div>
     </div>
 @endsection
